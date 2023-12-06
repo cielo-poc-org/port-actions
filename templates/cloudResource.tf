20a9d13ceea895c10e67640c23ad9893115b8749
Guilherme Rodrigues <guilherme.rodrigues@estudante.ufscar.br>
2023-12-06 15:54:53 -0300

    Update cloudResource.tf

--- templates/cloudResource.tf
+++ templates/cloudResource.tf
@@ -1,7 +1,7 @@
 # cloudResource.tf
 
 resource "aws_s3_bucket" "bucket" {
-provider = aws.bucket_region
-name = "{{ bucket_name }}"
-acl = "{{ bucket_acl }}"
+  provider = aws.bucket_region
+  name = "{{ bucket_name }}"
+  acl = "{{ bucket_acl }}"
 }
