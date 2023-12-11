a067389c1b11a7e8483cf6bdb2a3e405a06d5cb2
Guilherme Rodrigues <guilherme.rodrigues@estudante.ufscar.br>
2023-12-11 15:22:03 -0300

    Update ec2template.tf

--- templates/ec2template.tf
+++ templates/ec2template.tf
@@ -1,22 +1,9 @@
-terraform { 
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.0"
-        }
-    }
-}
-
-provider "aws" {
-    region = "us-east-1"
-}
-
 resource "aws_instance" "artzibordi_instance" {
-    ami =  {{ ami }}
-    instance_type = {{ instance_type }}
+    ami =  "{{ ami }}"
+    instance_type = "{{ instance_type }}"
 
     tags = {
-        Name = {{ name }}
+        Name = "{{ name }}"
     }
 }
 
